6fa8b6acbf95678a5965d8f7efe5440859bfd267
RanaMuhammadIrshad <rmirshad@web.de>
2022-07-26 15:18:42 +0200

    Updates

---
+++ precache-manifest.c79e2e845de3ae40912ac936a6ba44d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38a742a2b5ae418d1e71e6331f8bcbe",
+    "url": "/Styling-React-Components-React-App/index.html"
+  },
+  {
+    "revision": "3890564f712645fbd9fc",
+    "url": "/Styling-React-Components-React-App/static/css/main.3ff2fb07.chunk.css"
+  },
+  {
+    "revision": "145fc0e427c886e9f46f",
+    "url": "/Styling-React-Components-React-App/static/js/2.a42d187f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Styling-React-Components-React-App/static/js/2.a42d187f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3890564f712645fbd9fc",
+    "url": "/Styling-React-Components-React-App/static/js/main.d805763c.chunk.js"
+  },
+  {
+    "revision": "63907065589371a3b3de",
+    "url": "/Styling-React-Components-React-App/static/js/runtime-main.2c459d4c.js"
+  }
+]);
